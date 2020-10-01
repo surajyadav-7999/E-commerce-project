03b2edc57485610540c992e4d5b8ea2c9b5e8c42
surajyadav-7999 <72115850+surajyadav-7999@users.noreply.github.com>
2020-10-01 10:23:09 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,151 @@
+<div id="footer"><!-- #footer Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="row"><!-- row Begin -->
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+               
+               <h4>Pages</h4>
+                
+                <ul><!-- ul Begin -->
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="customer/my_account.php">My Account</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr>
+                
+                <h4>User Section</h4>
+                
+                <ul><!-- ul Begin -->
+                     <?php 
+                           
+                           if(!isset($_SESSION['customer_email'])){
+                               
+                               echo"<a href='checkout.php'>Login</a>";
+                               
+                           }else{
+                               
+                              echo"<a href='customer/my_account.php?my_orders'>My Account</a>"; 
+                               
+                           }
+                           
+                           ?>
+                    <li><a href="customer_register.php">Register</a></li>
+                    <li><a href="terms.php">Terms & Conditions</a></li>
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg hidden-sm">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="com-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Top Products Categories</h4>
+                
+                <ul><!-- ul Begin -->
+                   
+                       <?php 
+                    
+                        $get_p_cats = "select * from product_categories";
+                        $run_p_cats = mysqli_query($con,$get_p_cats);
+                    while($row_p_cats=mysqli_fetch_array($run_p_cats)){
+                            $p_cat_id = $row_p_cats['p_cat_id'];
+                            $p_cat_title = $row_p_cats['p_cat_title'];
+                            echo "
+                            <li>
+                                <a href='shop.php?p_cat=$p_cat_id'>
+                                $p_cat_title
+                                    </a>
+                                </li>
+                            ";
+                            }
+                    ?>
+                        
+                </ul><!-- ul Finish -->
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+                
+                <h4>Find Us</h4>
+                
+                <p><!-- p Start -->
+                    
+                    <strong>OM INFOTECH</strong>
+                    <br/>Mumbai
+                    <br/>9082-205797
+                    <br/>sgy2421999@gmail.com
+                    <br/><strong>Mr.Suraj</strong>
+                    
+                </p><!-- p Finish -->
+                
+                <a href="contact.php">Check Our Contact Page</a>
+                
+                <hr class="hidden-md hidden-lg">
+                
+            </div><!-- col-sm-6 col-md-3 Finish -->
+            
+            <div class="col-sm-6 col-md-3">
+                
+                <h4>Get The News</h4>
+                
+                <p class="text-muted">
+                    Dont miss our latest update products.
+                </p>
+                
+                <form action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=OmInfotech', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" method="post"><!-- form begin -->
+                    <div class="input-group"><!-- input-group begin -->
+                        
+                        <input type="text" class="form-control" name="email">
+        <input type="hidden" value="OmInfotech" name="uri"/><input type="hidden" name="loc" value="en_US"/>
+                        <span class="input-group-btn"><!-- input-group-btn begin -->
+                            
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                            
+                        </span><!-- input-group-btn Finish -->
+                        
+                    </div><!-- input-group Finish -->
+                </form><!-- form Finish -->
+                
+                <hr>
+                
+                <h4>Keep In Touch</h4>
+                
+                <p class="social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-google-plus"></a>
+                    <a href="#" class="fa fa-envelope"></a>
+                </p>
+                
+            </div>
+        </div><!-- row Finish -->
+
+   </div><!-- container Finish -->
+</div><!-- #footer Finish -->
+<div class="container-fluid">
+   <div class="col-lg-12">
+    
+       <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15072.236408312481!2d72.83506047444972!3d19.192620773671482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b71d7f8e97fb%3A0x43d93ad2423eb46e!2sOm%20Infotech!5e0!3m2!1sen!2sin!4v1579417860064!5m2!1sen!2sin" width="1300" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe></p>
+    </div>
+    
+</div>
+
+
+<div id="copyright"><!-- #copyright Begin -->
+    <div class="container"><!-- container Begin -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-left">&copy; 2019 S-Y Store All Rights Reserve</p>
+            
+        </div><!-- col-md-6 Finish -->
+        <div class="col-md-6"><!-- col-md-6 Begin -->
+            
+            <p class="pull-right">Theme by: <a href="#">SURAJ</a></p>
+            
+        </div><!-- col-md-6 Finish -->
+    </div><!-- container Finish -->
+</div><!-- #copyright Finish -->
